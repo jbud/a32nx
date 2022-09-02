090934c2ff11004c53f2517f9f6fca0a2f17b9fa
BBK <22713769+BlueberryKing@users.noreply.github.com>
2022-09-02 23:03:04 +0200

    Incorporate final descent segment in approach

--- src/fmgc/src/flightplanning/ManagedFlightPlan.ts
+++ src/fmgc/src/flightplanning/ManagedFlightPlan.ts
@@ -1191,6 +1191,8 @@ export class ManagedFlightPlan {
                     } else {
                         this.destinationAirfield.additionalData.annotation = approachName;
                     }
+
+                    this.destinationAirfield.verticalAngle = lastLeg.verticalAngle
                 }
 
                 // Clear discontinuity before destination, if any
